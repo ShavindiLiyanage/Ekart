89d301ff80907a1292c2bbf7cdc4f2027d64f714
Shavi678 <ssanthupamaliyanage@gmail.com>
2024-05-21 14:53:04 +0530

    @ViewChild() decorator

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-container',
@@ -49,6 +50,8 @@ export class ContainerComponent {
   listOfString: string[] = ['Mark','Steve','Merry','John','Sarah']
 
   searchText: string = '';
+
+  @ViewChild(ProductListComponent) productListComponent: ProductListComponent;
 
   setSearchText(value: string){
     this.searchText = value;
